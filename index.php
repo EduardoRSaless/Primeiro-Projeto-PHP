7d3640db9114022f1f65fea64cc5ad6e6dd608ef
Dev Edu <159092591+EduardoRSaless@users.noreply.github.com>
2026-04-23 18:25:28 -0300

    Feat array in index

--- index.php
+++ index.php
@@ -1,63 +1,109 @@
 <!doctype html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Meu Portifolio</title>
 </head>
 <body>
-<h1>
-    <?= "Portifolio do Eduardo"; ?>
-</h1>
+    <h1>
+        <?= "Portifolio do Eduardo"; ?>
+    </h1>
 
-<p>
-    <?php
-    echo "Lorem ipsum dui curae quisque litora lobortis nisi pulvinar risus gravida, hac ornare arcu semper diam sociosqu curabitur vulputate. placerat class ac lectus scelerisque vestibulum vitae mollis, imperdiet class curae massa justo ut et, rhoncus laoreet mattis dictum tincidunt habitant. ut pretium interdum sit posuere consequat eros augue, nullam non neque laoreet pretium dictum adipiscing platea, aptent ut aliquam vel cursus sem. sollicitudin nam condimentum arcu et cras bibendum in fusce, fermentum urna risus nisi quisque semper netus etiam, pellentesque cursus massa et sollicitudin cursus et. faucibus sapien nullam habitant velit fames pharetra lacinia, nec aptent eget et sit vitae, congue ullamcorper gravida id orci ut."
-    ?>
-</p>
-
-
-<?php
-$projeto = "Meu portifolio";
-$finalizado = true;
-$dataDoProjeto = " 21/04/2026";
-$descricao = "Meu primeiro portfolio em PHP!";
-$ano = "2020"
-
-$projetos = [
-        "Meu Portifolio",
-        "Lista de Tarefas",
-        "Lista de Projetos",
-        "Controle de Leitura de Livros",
-]
-?>
-
-<hr>
-
-<div>
-    <h1><?= $projeto . $dataDoProjeto ?></h1>
-    <p><?= $descricao ?></p>
-</div>
-
-<div>
-    <?php if (!$finalizado): ?>
-        <span style="color: green"> Finalizado ✅ </span>
-    <?php else: ?>
-        <span style="color: red"> Não finalizado ❌</span>
-    <?php endif; ?>
-
-</div>
-<hr>
-<div
-        <?php if ((2024 - $ano) > 2): ?>
-            style="background-color: burlywood"
-        <?php endif; ?>
->
     <p>
         <?php
         echo "Lorem ipsum dui curae quisque litora lobortis nisi pulvinar risus gravida, hac ornare arcu semper diam sociosqu curabitur vulputate. placerat class ac lectus scelerisque vestibulum vitae mollis, imperdiet class curae massa justo ut et, rhoncus laoreet mattis dictum tincidunt habitant. ut pretium interdum sit posuere consequat eros augue, nullam non neque laoreet pretium dictum adipiscing platea, aptent ut aliquam vel cursus sem. sollicitudin nam condimentum arcu et cras bibendum in fusce, fermentum urna risus nisi quisque semper netus etiam, pellentesque cursus massa et sollicitudin cursus et. faucibus sapien nullam habitant velit fames pharetra lacinia, nec aptent eget et sit vitae, congue ullamcorper gravida id orci ut."
         ?>
     </p>
 
+
+    <?php
+    $projeto = "Meu portifolio";
+    $finalizado = true;
+    $dataDoProjeto = " 21/04/2026";
+    $descricao = "Meu primeiro portfolio em PHP!";
+    $ano = "2020";
+    $projetos = [
+        [
+            "titulo" => "Meu portifolio",
+            "finalizado" => false,
+            "data" => "2026-0-22",
+            "descricao" => "Meu primeiro portfolio! Escrito em PHP e Html"
+        ],
+    ];
+
+    ?>
+
+    <hr>
+
+    <ul>
+
+        <?php foreach ($projetos as $projeto): ?>
+            <div <?php if ((2024 - $ano) > 2): ?> style="background-color: burlywood" <?php endif; ?>>
+
+            <h1><?= $projeto['titulo'] . $dataDoProjeto ?></h1>
+            <?php
+            echo "{$projeto['titulo']}";
+            echo "{$projeto['data']}";
+            echo "{$projeto['descricao']}";
+            ?>
+
+
+            <p><?= $descricao ?></p>
+
+            <div>
+                <?php if (!$finalizado): ?>
+                    <span style="color: green"> Finalizado ✅ </span>
+                <?php else: ?>
+                    <span style="color: red"> Não finalizado ❌</span>
+                <?php endif; ?>
+            </div>
+
+
+            
+                <p>
+                    <?php
+                    echo "Lorem ipsum dui curae quisque litora lobortis nisi pulvinar risus gravida, hac ornare arcu semper diam sociosqu curabitur vulputate. placerat class ac lectus scelerisque vestibulum vitae mollis, imperdiet class curae massa justo ut et, rhoncus laoreet mattis dictum tincidunt habitant. ut pretium interdum sit posuere consequat eros augue, nullam non neque laoreet pretium dictum adipiscing platea, aptent ut aliquam vel cursus sem. sollicitudin nam condimentum arcu et cras bibendum in fusce, fermentum urna risus nisi quisque semper netus etiam, pellentesque cursus massa et sollicitudin cursus et. faucibus sapien nullam habitant velit fames pharetra lacinia, nec aptent eget et sit vitae, congue ullamcorper gravida id orci ut."
+                    ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
+
+    <hr>
+    <?php
+    $lista = "Lista de Tarefas";
+    $livros = [
+            [
+        "livro1" => "João e Maria",
+        "livro2" => "Freud explica",
+        "livro3" => "O Eu e o Inconsciente"
+        ]
+    ];
+    ?>
+
+    <?php foreach ($livros as $livro): ?>
+
+    <div <?php if ((2024 - $ano) > 2): ?> style="background-color: burlywood" <?php endif; ?>>
+        <h1><?php echo $lista ?></h1>
+        <div>
+            <?php if ($finalizado): ?>
+                    <span style="color: green">Finalizado ✅</span>
+               <?php else: ?>
+                    A<span style="color: red"> Não finalizado ❌</span>
+               <?php endif; ?>
+
+        </div>
+
+        <div>
+
+            <?php
+            echo "{$livro['livro1']}";
+            echo "{$livro['livro2']}";
+            echo "{$livro['livro3']}";
+            ?>
+
+    </ul>
+        </div>
+    <?php endforeach; ?>
 </body>
 </html>
